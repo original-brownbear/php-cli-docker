b237bcc3c2fa383a085a6de65cd54bd363107cee
Armin Braun <armin.b@icanlocalize.com>
2015-12-24 13:58:14 +0100

    added xdebug version

--- release.sh
+++ release.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+cd ./no-xdebug
 docker build -t originalbrownbear/php:7-cli-phpunit .
 docker push originalbrownbear/php:7-cli-phpunit
+cd ../xdebug
+docker build -t originalbrownbear/php:7-cli-phpunit-xdebug .
+docker push originalbrownbear/php:7-cli-phpunit-xdebug
