f295096fc4bbf8e67ef494fe2b2366ff7b37ddbc
Armin Braun <armin.b@icanlocalize.com>
2015-12-24 15:33:59 +0100

    added remote autostart to xdebug version

--- release.sh
+++ release.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-cd ./no-xdebug
+cd ./7-no-xdebug
 docker build -t originalbrownbear/php:7-cli-phpunit .
 docker push originalbrownbear/php:7-cli-phpunit
-cd ../xdebug
+cd ../7-xdebug
 docker build -t originalbrownbear/php:7-cli-phpunit-xdebug .
 docker push originalbrownbear/php:7-cli-phpunit-xdebug
+cd ../5-xdebug
+docker build -t originalbrownbear/php:5-cli-phpunit-xdebug .
+docker push originalbrownbear/php:5-cli-phpunit-xdebug
